3f61b2d14bfa9483e09bcab8c46a069273927243
Artjom Makarov <armaka@ttu.ee>
2021-04-05 01:51:27 +0300

    Test when first three are NORM no bonus

--- src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
+++ src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
@@ -9,8 +9,8 @@ public class TietoBowling {
         bowling.init("Artjom","Alina");
         bowling.playerNames.forEach(System.out::println);
         bowling.throwBall(1,5, "Artjom");
-        bowling.throwBall(5,5, "Artjom");
-        bowling.throwBall(5,5, "Artjom");
+        bowling.throwBall(1,5, "Artjom");
+        bowling.throwBall(1,5, "Artjom");
         bowling.throwBall(5,5, "Artjom");
         bowling.throwBall(5,5, "Artjom");
         bowling.throwBall(5,5, "Artjom");
