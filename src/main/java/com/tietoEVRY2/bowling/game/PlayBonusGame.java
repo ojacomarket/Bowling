b0c4f709e1abf3e9af67953bd232994d92e50276
Artjom Makarov <armaka@ttu.ee>
2021-04-05 23:34:04 +0300

    Shorten code for PlayBonusFrame

--- src/main/java/com/tietoEVRY2/bowling/game/PlayBonusGame.java
+++ src/main/java/com/tietoEVRY2/bowling/game/PlayBonusGame.java
@@ -1,11 +1,11 @@
 package com.tietoEVRY2.bowling.game;
 
-import com.tietoEVRY2.bowling.util.BonusFrameSuperCheck;
-import com.tietoEVRY2.bowling.util.ManageFrameStatusesBonus;
 import lombok.AllArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.tietoEVRY2.bowling.util.ManageFrameStatusesBonus.*;
 
 @AllArgsConstructor
 public class PlayBonusGame {
@@ -14,16 +14,11 @@ public class PlayBonusGame {
     public int frameNr;
 
     public void playBonusFrame(int roll1, int roll2, int roll3) {
-        System.out.println(frameNr);
-        List<FrameStatus> localRollFrameStatuses = new ArrayList<>();
-        db.totalScore += roll1;
-        frames.add(new Frames());
-        frames.get(frameNr).roll1 = roll1;
-        ManageFrameStatusesBonus.checkFirstRoll(roll1, localRollFrameStatuses);
 
-        frames.get(frameNr).roll2 = roll2;
-        db.totalScore += roll2;
-        ManageFrameStatusesBonus.checkSecondRoll(roll1, roll2, localRollFrameStatuses);
-        BonusFrameSuperCheck.superCheck(localRollFrameStatuses, db, frames, frameNr, roll1, roll2, roll3);
+        List<FrameStatus> localRollFrameStatuses = new ArrayList<>();
+
+        throwRoll1(db, roll1, frames, frameNr, localRollFrameStatuses);
+
+        throwRoll2and3(db, roll1, roll2, roll3, frames, frameNr, localRollFrameStatuses);
     }
 }
