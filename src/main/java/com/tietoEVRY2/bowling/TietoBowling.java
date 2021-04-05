a7cb843f1239027565ac9896684ee3c7f11f9140
Artjom Makarov <armaka@ttu.ee>
2021-04-06 00:26:27 +0300

    Implementation

--- src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
+++ src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
@@ -2,34 +2,34 @@ package com.tietoEVRY2.bowling;
 
 import com.tietoEVRY2.bowling.game.BowlingGame;
 import com.tietoEVRY2.bowling.game.ConsoleBowling;
-import com.tietoEVRY2.bowling.util.CheckPlayerExistance;
-import com.tietoEVRY2.bowling.util.HandleFrameCombosBonus;
 
 public class TietoBowling {
     public static void main(String[] args) {
         BowlingGame bowling = new BowlingGame();
         bowling.init("Artjom", "Alina");
+        bowling.throwBall(1, 9, "Artjom");
+
+        bowling.throwBall(5, 0, "Artjom");
+
+        bowling.throwBall(1, 0, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(1, 0, "Artjom");
+
         bowling.throwBall(10, 0, "Artjom");
 
         bowling.throwBall(10, 0, "Artjom");
 
-        bowling.throwBall(10, 0, "Artjom");
+        bowling.throwBall(0, 5, "Artjom");
 
-        bowling.throwBall(10, 0, "Artjom");
+        bowling.throwBall(1, 0,"Artjom");
 
-        bowling.throwBall(10, 0, "Artjom");
-
-        bowling.throwBall(10, 0, "Artjom");
-
-        bowling.throwBall(10, 0, "Artjom");
-
-        bowling.throwBall(5, 5, "Artjom");
-
-        bowling.throwBall(10, 0, "Artjom");
-
-        bowling.throwBall(4, 6, 2, "Artjom");
+        bowling.throwBall(0, 1, "Artjom");
 
         ConsoleBowling playBowlingInConsole = new ConsoleBowling();
         playBowlingInConsole.showFullPlayerTable("Artjom", bowling.scoreBoards);
+
+       // System.out.println(bowling.showCurrentScore(bowling, "Artjom"));
     }
 }
