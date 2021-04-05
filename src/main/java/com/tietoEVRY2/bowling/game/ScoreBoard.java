df909ba4b0390809df6491bbf152cc0884d369bb
Artjom Makarov <armaka@ttu.ee>
2021-04-05 12:40:38 +0300

    Fix logic for strike normal

--- src/main/java/com/tietoEVRY2/bowling/game/ScoreBoard.java
+++ src/main/java/com/tietoEVRY2/bowling/game/ScoreBoard.java
@@ -58,11 +58,8 @@ public class ScoreBoard {
         if (frameTracker >= 1) {
                 if (FrameCombinations.strike_spare(frameTracker,tdb)) {
                     HandleFrameCombos.handle_strike_spare(frames,frameTracker,tdb);
-                } else if (tdb.gameStatus.get(frameTracker - 1) == STATUS.STRIKE && tdb.gameStatus.get(frameTracker) == STATUS.NORMAL) {
-                    frames.get(frameTracker - 1).scoreToCount = tdb.totalScore;
-                    tdb.totalScore = frames.get(frameTracker - 1).scoreToCount + roll1 + roll2;
-                    frames.get(frameTracker).scoreToCount = tdb.totalScore;
-                    //totalScore.total += roll1 +roll2 + 10;
+                } else if (FrameCombinations.strike_normal(frameTracker,tdb)) {
+                    HandleFrameCombos.handle_strike_normal(frames,frameTracker,tdb,roll1,roll2);
                 } else if (tdb.gameStatus.get(frameTracker - 1) == STATUS.SPARE && tdb.gameStatus.get(frameTracker) == STATUS.NORMAL) {
                     frames.get(frameTracker - 1).scoreToCount = tdb.totalScore - roll2;
                     tdb.totalScore = frames.get(frameTracker - 1).scoreToCount + roll1 + roll2;
