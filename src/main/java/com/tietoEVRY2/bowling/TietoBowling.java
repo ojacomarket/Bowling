9da19bbb5da5dd30852b8c406e159b4af823b924
Artjom Makarov <armaka@ttu.ee>
2021-04-05 04:36:50 +0300

    Test 7th SPARE, 8th STRIKE, 9th NORM

--- src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
+++ src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
@@ -27,7 +27,7 @@ public class TietoBowling {
        bowling.throwBall(10,0, "Artjom");
         System.out.println(bowling.scoreBoards.get(0).getFrameTracker());
         System.err.println(bowling.scoreBoards.get(0).getCurrentScores().get(1).totalScore);
-        bowling.throwBall(10,1, 7, "Artjom");
+        bowling.throwBall(4,1, 0, "Artjom");
         System.out.println(bowling.scoreBoards.get(0).getFrameTracker());
         //System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
         //System.out.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
