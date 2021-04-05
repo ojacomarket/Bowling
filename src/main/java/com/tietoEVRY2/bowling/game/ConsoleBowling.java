9b62f74dc61997d788d3ad8333d149c6651f2317
Artjom Makarov <armaka@ttu.ee>
2021-04-05 23:25:31 +0300

    Shorten Console app

--- src/main/java/com/tietoEVRY2/bowling/game/ConsoleBowling.java
+++ src/main/java/com/tietoEVRY2/bowling/game/ConsoleBowling.java
@@ -1,47 +1,24 @@
 package com.tietoEVRY2.bowling.game;
 
-import com.tietoEVRY2.bowling.util.*;
-import com.tietoEVRY2.bowling.util.console.ConsoleDrawer;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.List;
 
+import static com.tietoEVRY2.bowling.util.CheckPlayerExistance.checkExistance;
+import static com.tietoEVRY2.bowling.util.console.ConsoleDrawer.*;
+
 public class ConsoleBowling {
 
     public void showFullPlayerTable(String player, List<ScoreBoard> scoreBoardsIncoming) {
-        ScoreBoard realPlayer = CheckPlayerExistance.checkExistance(player, scoreBoardsIncoming);
+        ScoreBoard realPlayer = checkExistance(player, scoreBoardsIncoming);
         if (ObjectUtils.equals(realPlayer, null)) {
             return;
         }
-        //System.out.format("+-----------------+---------+--------+--------+%n");
-        if (scoreBoardsIncoming.get(scoreBoardsIncoming.indexOf(realPlayer)).getFrameTracker() > 8) {
-            System.out.format("+-----------------+---------+--------+--------+--------+%n");
-            System.out.format("| %s          | Roll1   | Roll2  | Roll3  | Total  |%n", "Artjom");
-        } else {
-            System.out.format("+-----------------+---------+--------+--------+%n");
-            System.out.format("| %s          | Roll1   | Roll2  | Total  |%n", "Artjom");
-        }
-        if (scoreBoardsIncoming.get(scoreBoardsIncoming.indexOf(realPlayer)).getFrameTracker() > 8) {
-            System.out.format("+-----------------+---------+--------+--------+--------+%n");
-        }
-        else {
-            System.out.format("+-----------------+---------+--------+--------+%n");
-        }
-        for (int throwsMade = 0; throwsMade < scoreBoardsIncoming
-                .get(scoreBoardsIncoming.indexOf(realPlayer))
-                .getFrames().size(); throwsMade++) {
-            if (scoreBoardsIncoming.get(scoreBoardsIncoming.indexOf(realPlayer)).getFrameTracker() > 8) {
-                ConsoleDrawer.drawToConsoleOnLastFrame(realPlayer, throwsMade);
-            } else {
-                ConsoleDrawer.drawToConsole(realPlayer, throwsMade);
 
-            }
-        }
-        if (scoreBoardsIncoming.get(scoreBoardsIncoming.indexOf(realPlayer)).getFrameTracker() > 8) {
-            System.out.format("+-----------------+---------+--------+--------+--------+%n");
-        }
-        else {
-            System.out.format("+-----------------+---------+--------+--------+%n");
-        }
+        customizeHeaderAndColumns(scoreBoardsIncoming, realPlayer);
+
+        customizeBody(scoreBoardsIncoming, realPlayer);
+
+        customizeFooter(scoreBoardsIncoming, realPlayer);
     }
 }
