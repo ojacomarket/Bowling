bdcce2ab386edaf0001cf95a10e48e0b7f2b3eb3
Artjom Makarov <armaka@ttu.ee>
2021-04-05 03:59:28 +0300

    Test 8th is SPARE and 9th is all three STRIKES

--- src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
+++ src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
@@ -27,7 +27,7 @@ public class TietoBowling {
        bowling.throwBall(5,5, "Artjom");
         System.out.println(bowling.scoreBoards.get(0).getFrameTracker());
         System.err.println(bowling.scoreBoards.get(0).getCurrentScores().get(1).totalScore);
-        bowling.throwBall(5,1, 2, "Artjom");
+        bowling.throwBall(10,10, 10, "Artjom");
         System.out.println(bowling.scoreBoards.get(0).getFrameTracker());
         //System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
         //System.out.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
