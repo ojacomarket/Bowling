0dea7bea829738c6a6b45487926ce9cc0a1639d6
Artjom Makarov <armaka@ttu.ee>
2021-04-05 04:39:00 +0300

    Remove unused lines

--- src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
+++ src/main/java/com/tietoEVRY2/bowling/TietoBowling.java
@@ -6,33 +6,27 @@ import com.tietoEVRY2.bowling.game.ConsoleBowling;
 public class TietoBowling {
     public static void main(String[] args) {
         BowlingGame bowling = new BowlingGame();
-        bowling.init("Artjom","Alina");
-        bowling.playerNames.forEach(System.out::println);
-         bowling.throwBall(10,0, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        bowling.throwBall(10,0, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        bowling.throwBall(10,0, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        bowling.throwBall(10,0, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        bowling.throwBall(10,0, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        bowling.throwBall(10,0, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-       bowling.throwBall(10,0, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        bowling.throwBall(5,5, "Artjom");
-        System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-       bowling.throwBall(10,0, "Artjom");
-        System.out.println(bowling.scoreBoards.get(0).getFrameTracker());
-        System.err.println(bowling.scoreBoards.get(0).getCurrentScores().get(1).totalScore);
-        bowling.throwBall(4,1, 0, "Artjom");
-        System.out.println(bowling.scoreBoards.get(0).getFrameTracker());
-        //System.err.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        //System.out.println(bowling.scoreBoards.get(0).getFrames().get(0).scoreToCount);
-        //System.out.println(bowling.scoreBoards.get(0).getFrames().get(1).scoreToCount);
+        bowling.init("Artjom", "Alina");
 
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(5, 5, "Artjom");
+
+        bowling.throwBall(10, 0, "Artjom");
+
+        bowling.throwBall(4, 1, 0, "Artjom");
 
         ConsoleBowling playBowlingInConsole = new ConsoleBowling();
         playBowlingInConsole.showFullPlayerTable("Artjom", bowling.scoreBoards);
