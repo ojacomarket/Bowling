09d7ab78a8bb31f96a633b9d397dd3cd48160dc1
Artjom Makarov <armaka@ttu.ee>
2021-04-05 12:23:34 +0300

    Change method name

--- src/main/java/com/tietoEVRY2/bowling/util/HandleFrameCombos.java
+++ src/main/java/com/tietoEVRY2/bowling/util/HandleFrameCombos.java
@@ -6,7 +6,7 @@ import com.tietoEVRY2.bowling.game.TABLE_DATABASE;
 import java.util.List;
 
 public abstract class HandleFrameCombos {
-    public static void handle_strike_strike (List<Frames> frames, int frameTracker, TABLE_DATABASE tdb) {
+    public static void handle_strike_strike_strike(List<Frames> frames, int frameTracker, TABLE_DATABASE tdb) {
         frames.get(frameTracker - 2).scoreToCount = tdb.totalScore;
         tdb.totalScore = frames.get(frameTracker - 2).scoreToCount + 20;
     }
