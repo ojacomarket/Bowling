1443d81e0dd481fc6c15dfce100252bd4ce0bb07
Artjom Makarov <armaka@ttu.ee>
2021-04-05 13:32:08 +0300

    Add bonus cases

--- src/main/java/com/tietoEVRY2/bowling/util/FrameCombinations.java
+++ src/main/java/com/tietoEVRY2/bowling/util/FrameCombinations.java
@@ -3,8 +3,79 @@ package com.tietoEVRY2.bowling.util;
 import com.tietoEVRY2.bowling.game.STATUS;
 import com.tietoEVRY2.bowling.game.TABLE_DATABASE;
 
+import java.util.List;
+
 public abstract class FrameCombinations {
-    public static boolean strike_strike (int frameTracker, TABLE_DATABASE tdb) {
-        return frameTracker > 1 && tdb.gameStatus.get(frameTracker - 2) == STATUS.STRIKE && tdb.gameStatus.get(frameTracker-1) == STATUS.STRIKE;
+    public static boolean strike_strike(int frameTracker, TABLE_DATABASE tdb) {
+        return frameTracker > 1 && tdb.gameStatus.get(frameTracker - 2) == STATUS.STRIKE && tdb.gameStatus.get(frameTracker - 1) == STATUS.STRIKE;
+    }
+
+    public static boolean strike_spare(int frameTracker, TABLE_DATABASE tdb) {
+        return tdb.gameStatus.get(frameTracker - 1) == STATUS.STRIKE && tdb.gameStatus.get(frameTracker) == STATUS.SPARE;
+    }
+
+    public static boolean strike_normal(int frameTracker, TABLE_DATABASE tdb) {
+        return tdb.gameStatus.get(frameTracker - 1) == STATUS.STRIKE && tdb.gameStatus.get(frameTracker) == STATUS.NORMAL;
+    }
+
+    public static boolean spare_normal(int frameTracker, TABLE_DATABASE tdb) {
+        return tdb.gameStatus.get(frameTracker - 1) == STATUS.SPARE && tdb.gameStatus.get(frameTracker) == STATUS.NORMAL;
+    }
+
+    public static boolean spare_strike(int frameTracker, TABLE_DATABASE tdb) {
+        return tdb.gameStatus.get(frameTracker - 1) == STATUS.SPARE && tdb.gameStatus.get(frameTracker) == STATUS.STRIKE;
+    }
+
+    public static boolean spare_spare(int frameTracker, TABLE_DATABASE tdb) {
+        return tdb.gameStatus.get(frameTracker - 1) == STATUS.SPARE && tdb.gameStatus.get(frameTracker) == STATUS.SPARE;
+    }
+
+    public static boolean strike_bonus_for_spare(List<STATUS> localRollStatus) {
+        return localRollStatus.get(0) == STATUS.STRIKE || localRollStatus.get(1) == STATUS.SPARE;
+    }
+
+    public static boolean strike_strike_bonus_for_strike(List<STATUS> localRollStatus, int frameNr, TABLE_DATABASE db) {
+        return db.gameStatus.get(frameNr - 2) == STATUS.STRIKE && db.gameStatus.get(frameNr - 1) == STATUS.STRIKE &&
+                localRollStatus.get(0) == STATUS.STRIKE;
+    }
+
+    public static boolean strike_bonus_for_strike(List<STATUS> localRollStatus, int frameNr, TABLE_DATABASE db) {
+        return db.gameStatus.get(frameNr - 1) == STATUS.STRIKE && localRollStatus.get(0) == STATUS.STRIKE;
+    }
+
+    public static boolean strike_strike_bonus_for_two_normals_or_single_spare(List<STATUS> localRollStatus, int frameNr, TABLE_DATABASE db) {
+        return db.gameStatus.get(frameNr - 2) == STATUS.STRIKE &&
+                db.gameStatus.get(frameNr - 1) == STATUS.STRIKE &&
+                localRollStatus.get(0) == STATUS.NORMAL &&
+                (localRollStatus.get(1) == STATUS.NORMAL ||
+                        localRollStatus.get(1) == STATUS.SPARE);
+    }
+
+    public static boolean spare_bonus_for_spare(List<STATUS> localRollStatus, int frameNr, TABLE_DATABASE db) {
+        return db.gameStatus.get(frameNr - 1) == STATUS.SPARE &&
+                localRollStatus.get(1) == STATUS.SPARE;
+    }
+
+    public static boolean spare_bonus_for_normal(List<STATUS> localRollStatus, int frameNr, TABLE_DATABASE db) {
+        return db.gameStatus.get(frameNr - 1) == STATUS.SPARE &&
+                localRollStatus.get(1) == STATUS.NORMAL;
+    }
+
+    public static boolean bonus_for_normal(List<STATUS> localRollStatus, int frameNr, TABLE_DATABASE db) {
+        return localRollStatus.get(0) == STATUS.NORMAL && localRollStatus.get(1) == STATUS.NORMAL &&
+                db.gameStatus.get(frameNr - 1) == STATUS.NORMAL;
+    }
+
+    public static boolean strike_bonus_for_normal_or_spare(List<STATUS> localRollStatus, int frameNr, TABLE_DATABASE db) {
+        return db.gameStatus.get(frameNr - 1) == STATUS.STRIKE && localRollStatus.get(0) == STATUS.NORMAL &&
+                (localRollStatus.get(1) == STATUS.NORMAL || localRollStatus.get(1) == STATUS.SPARE);
+    }
+
+    public static boolean bonus_for_strike(List<STATUS> localRollStatus) {
+        return localRollStatus.get(0) == STATUS.STRIKE;
+    }
+
+    public static boolean bonus_for_spare(List<STATUS> localRollStatus) {
+        return localRollStatus.get(0) == STATUS.NORMAL && localRollStatus.get(1) == STATUS.SPARE;
     }
 }
